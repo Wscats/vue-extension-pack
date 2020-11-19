8813c1fd9f3be9bf8a13c55f90bcf66c95a8d495
enoyao <enoyao@tencent.com>
2020-11-19 15:54:21 +0800

    增加测试文件夹
    增加测试文件夹

---
+++ test/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <view class="abc" v-if="step === 1">
+        <p>123</p>
+        <text class="abc" v-if="step === 1">12 3</text>
+    </view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
